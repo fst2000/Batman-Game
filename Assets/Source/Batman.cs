28e96e847944545eb8004e2a66b75be1b71b556c
fst2000 <fst.18.work@gmail.com>
2022-11-19 16:21:58 +0300

    Snow particles added, wings size and rigidbody.centerOfMass values edited

--- Assets/Source/Batman.cs
+++ Assets/Source/Batman.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [System.Serializable]
 public class Batman
 {
-    [SerializeField] float walkSpeed = 2f;
-    [SerializeField] float runSpeed = 4f;
+    [SerializeField] float walkSpeed = 2.5f;
+    [SerializeField] float runSpeed = 5f;
     [SerializeField] float wingRotationAngle = 20f;
-    [SerializeField] Vector3 centerOfMass = new Vector3(0, 1.1f, .2f);
+    [SerializeField] Vector3 centerOfMass = new Vector3(0, 1f, 0.2f);
     PlayerInput playerInput;
     Rigidbody rigidbody;    
     Animator animator;
     Transform transform;
+    bool isFlyingStarted;
 
     Wing[] wings;
     Wing wingL;
@@ -30,10 +32,10 @@ public class Batman
         rigidbody.angularDrag = 0.5f;
         transform = gameObject.transform;
 
-        wingL = new Wing(Vector3.ClampMagnitude(new Vector3(1, -1, -10), 1), new Vector3(-1.5f,1,0), 2f,1.5f);
-        wingR = new Wing(Vector3.ClampMagnitude(new Vector3(-1, -1, -10), 1), new Vector3(1.5f,1,0), 2f,1.5f);
-        wingBack = new Wing(Vector3.back, Vector3.zero, 0.5f, 1f);
-        wingStabilize = new Wing(Vector3.right, new Vector3(0,0,0.5f), .5f, .5f);
+        wingL = new Wing(Vector3.ClampMagnitude(new Vector3(1, -1, -10), 1), new Vector3(-1f,1,0), 2f,1f);
+        wingR = new Wing(Vector3.ClampMagnitude(new Vector3(-1, -1, -10), 1), new Vector3(1f,1,0), 2f,1f);
+        wingBack = new Wing(Vector3.back, Vector3.zero, 1f, 0.5f);
+        wingStabilize = new Wing(Vector3.right, new Vector3(0,0,0.5f), .25f, .5f);
         wings = new Wing[] {wingL,wingR, wingBack ,wingStabilize};
     }
     public void Update()
@@ -47,6 +49,7 @@ public class Batman
     }
     void Move(float moveSpeed)
     {
+        isFlyingStarted = true;
         rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
         bool isMoving = playerInput.WalkInput != Vector3.zero;
         Vector3 velocity = rigidbody.velocity;
@@ -62,12 +65,17 @@ public class Batman
     }
     void Fly()
     {
+        if (isFlyingStarted)
+        {
+            transform.rotation = transform.rotation * Quaternion.Euler(100, 0, 0);
+        }
+        isFlyingStarted = false;
         rigidbody.constraints = RigidbodyConstraints.None;
         animator.SetBool("isMoving", false);
         animator.SetBool("isFlying", true);
         animator.SetFloat("flyBlend", -playerInput.MoveVertical);
-        rigidbody.AddRelativeTorque(0, 0, 60 * -playerInput.MoveHorizontal);
-        wingBack.UpdateRotation(new Vector3(-playerInput.MoveVertical * wingRotationAngle,0, 0));
+        rigidbody.AddRelativeTorque(0, 0, rigidbody.velocity.magnitude * 2f * -playerInput.MoveHorizontal);
+        wingBack.UpdateRotation(new Vector3(-playerInput.MoveVertical * wingRotationAngle, 0, 0));
 
         foreach (Wing wing in wings)
         {
@@ -77,7 +85,6 @@ public class Batman
             Vector3 globalVelocity = rigidbody.GetPointVelocity(wingGlobalPosition);
             rigidbody.AddForceAtPosition(globalNormal * Vector3.Dot(globalNormal, -globalVelocity) * wing.GetSquare() * globalVelocity.magnitude, wingGlobalPosition);
         }
-
     }
     public void OnDrawGizmos()
     {
